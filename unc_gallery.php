fb563436b44a59a71ae11558051abaa60e883f2f
uncovery <minecraft@uncovery.me>
2015-07-08 14:54:12 +0800

    add initial file

---
+++ unc_gallery.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: UNC Gallery
+Plugin URI:  https://github.com/uncovery/unc_gallery
+Description: Wordpress-plugin for a simple, self-generating, date-based gallery
+Version:     0.1
+Author:      Uncovery
+Author URI:  http://uncovery.net
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
