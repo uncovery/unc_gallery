b90efea017bdac164853000779e6dd0e9402a931
uncovery <minecraft@uncovery.me>
2015-07-09 16:15:45 +0800

    integrate upload dialogue

--- unc_gallery.php
+++ unc_gallery.php
@@ -93,6 +93,7 @@ function unc_gallery_admin_init() {
  * Helptext for the settings menu
  */
 function umc_gallery_settings_callback() {
+    wp_enqueue_media();
     echo "Please set your options:";
 }
 
@@ -101,6 +102,34 @@ function unc_gallery_field_one_callback() {
     $color = esc_attr( $settings['color'] );
     echo "<input type='text' name='unc_gallery_setting[color]' value='$color' />";
 
+    ?>
+        <label for="image_url">Upload photos</label>
+        <input type="text" name="image_url" id="image_url" class="regular-text">
+        <input type="button" name="upload-btn" id="upload-btn" class="button-secondary" value="Upload Image">
+    <script type="text/javascript">
+    jQuery(document).ready(function($){
+        $('#upload-btn').click(function(e) {
+            e.preventDefault();
+            var image = wp.media({
+                title: 'Upload Images',
+                // mutiple: true if you want to upload multiple files at once
+                multiple: true
+            }).open()
+            .on('select', function(e){
+                // This will return the selected image from the Media Uploader, the result is an object
+                var uploaded_image = image.state().get('selection').first();
+                // We convert uploaded_image to a JSON object to make accessing it easier
+                // Output to the console uploaded_image
+                console.log(uploaded_image);
+                var image_url = uploaded_image.toJSON().url;
+                // Let's assign the url value to the input field
+                $('#image_url').val(image_url);
+            });
+        });
+    });
+    </script>
+
+    <?php
 }
 
 function unc_gallery_options() {
