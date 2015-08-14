a4ca451c7eff6d2a5439fd19203c0242b83afee9
uncovery <minecraft@uncovery.me>
2015-08-14 12:07:34 +0800

    new config options

--- unc_config.inc.php
+++ unc_config.inc.php
@@ -6,13 +6,10 @@ if (!defined('WPINC')) {
 
 global $WPG_CONFIG;
 
-// thse should be transfered to the database so they can be changed in the admin settings.
-$WPG_CONFIG['gallery_path'] = plugin_dir_path( __FILE__ ) . "galleries/default";
 // upload folders below above folder
 $WPG_CONFIG['upload'] = "/unc_gallery";
 
 $WPG_CONFIG['photos'] = "/photos";
-$WPG_CONFIG['invalid'] = "/invalid";
 $WPG_CONFIG['thumbnails'] = "/thumbs";
 
 $WPG_CONFIG['base_url'] = plugins_url();
@@ -22,4 +19,6 @@ $WPG_CONFIG['timezone'] = 'Asia/Hong_Kong';
 $WPG_CONFIG['thumbnail_size'] = 300;
 $WPG_CONFIG['thumbnail_ext'] = 'PNG';
 
-$WPG_CONFIG['valid_filetypes'] = array("image/gif", "image/jpeg", "image/png",);
+$WPG_CONFIG['valid_filetypes'] = array("image/gif" => 'gif', "image/jpeg" => 'jpg', "image/png" => 'png',);
+
+$WPG_CONFIG['offset'] = "-12 hours";
