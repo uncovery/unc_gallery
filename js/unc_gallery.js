c7ff0926edfad4d1969d55b876679e6d08b97797
uncovery <minecraft@uncovery.me>
2015-09-02 15:14:30 +0800

    format the currently selected date differently

--- js/unc_gallery.js
+++ js/unc_gallery.js
@@ -7,14 +7,25 @@ function available(date) {
     iso = date.toISOString();
     ymd = iso.substring(0, 10);
     if (jQuery.inArray(ymd, availableDates) !== -1) {
-        return [true, "dateavailable",ymd + " has images"];
+        return [true, formatCurrentDate(ymd), ymd + " has images"];
     } else {
-        return [false,"dateunavailable","No images on " + ymd];
+        return [false, "dateunavailable", "No images on " + ymd];
+    }
+}
+
+// this parses the current iterated date and checks if it's the current displayed
+function formatCurrentDate(dateYmd) {
+    var query = window.location.search.substring(1);
+    if (query.search(dateYmd) > 0) {
+        return "dateavailable dateShown";
+    } else {
+        return "dateavailable";
     }
 }
 
 function openlink(dateText, inst) {
-    window.location = "?date=" + dateText;
+    // this needs to be improved. It needs to replace the string instead of duplicating it
+    window.location = window.location + "&unc_date=" + dateText;
 }
 
 Date.prototype.addMinutes= function(m){
