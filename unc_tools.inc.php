52ecf4d5fda831808b7d82a07d8676721cde02db
uncovery <minecraft@uncovery.me>
2015-09-02 13:54:22 +0800

    folder creation code

--- unc_tools.inc.php
+++ unc_tools.inc.php
@@ -4,11 +4,51 @@ if (!defined('WPINC')) {
     die;
 }
 
+/**
+ * Take a date string and create the respective folders
+ *
+ * @global type $WPG_CONFIG
+ * @param type $i
+ * @param type $date_str
+ * @return type
+ */
+function unc_date_folder_create($date_str) {
+    global $WPG_CONFIG;
+
+    $date_folders = array(false, "Y", "m", "d");
+    $dirPath =  WP_CONTENT_DIR . $WPG_CONFIG['upload'];
+    $date_obj = unc_datetime($date_str);
+    // substract 12 hours to get the correct date
+    $date_obj->modify($WPG_CONFIG['offset']);
+
+    $path_arr = array($WPG_CONFIG['photos'], $WPG_CONFIG['thumbnails']);
+    foreach ($path_arr as $img_folder) {
+        $base_folder = $dirPath . $img_folder;
+        foreach ($date_folders as $date_folder) {
+            if ($date_folder) {
+                $date_element = $date_obj->format($date_folder);
+                $base_folder .= DIRECTORY_SEPARATOR . "$date_element";
+            }
+            if (!file_exists($base_folder)) {
+                $mkdir_chk = mkdir($base_folder);
+                if (!$mkdir_chk) {
+                    echo "ERROR, could not create folder $base_folder<br>";
+                    return false;
+                } else {
+                    echo "Created folder $base_folder<br>";
+                }
+            }
+        }
+    }
+    return $date_obj;
+}
+
+
 /*
  * returns a date-time object with todays timezone
  * get a MySQL timestamp with $now = $date_now->format("Y-m-d H:i:s");
  */
-function unc_gallery_datetime($date = NULL) {
+function unc_datetime($date = NULL) {
     global $WPG_CONFIG;
 
     //if ($date != NULL) {
