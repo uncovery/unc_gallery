a874f2eed50f0300d4176478d7962df1fe0b16fe
uncovery <minecraft@uncovery.me>
2016-02-18 20:11:41 +0800

    fix JS

--- js/unc_gallery.js
+++ js/unc_gallery.js
@@ -1,17 +1,42 @@
 
-function available(date) {
-    off = date.getTimezoneOffset();
-    // adjust for timezone
-    off_inv = off * -1;
-    date.addMinutes(off_inv);
-    iso = date.toISOString();
-    ymd = iso.substring(0, 10);
-    if (jQuery.inArray(ymd, availableDates) !== -1) {
-        return [true, formatCurrentDate(ymd), ymd + " has images"];
-    } else {
-        return [false, "dateunavailable", "No images on " + ymd];
+    function datepicker_available(date) {
+        off = date.getTimezoneOffset();
+        // adjust for timezone
+        off_inv = off * -1;
+        date.addMinutes(off_inv);
+        iso = date.toISOString();
+        ymd = iso.substring(0, 10);
+        if (jQuery.inArray(ymd, availableDates) !== -1) {
+            return [true, formatCurrentDate(ymd), ymd + " has images"];
+        } else {
+            return [false, "dateunavailable", "No images on " + ymd];
+        }
     }
-}
+
+    function datepicker_select(dateText, inst) {
+        jQuery.ajax({
+            url: ajaxurl,
+            method: 'GET',
+            dataType: 'text',
+            data: {action: 'unc_gallery_datepicker', date: dateText},
+            complete: function (response) {
+                jQuery('#photobox').html(response.responseText);
+                jQuery('#photodate').html("Showing " + dateText);
+            },
+            error: function () {
+                 
+            }
+        });
+    }
+
+    function datepicker_ready(defaultdate) {
+        jQuery('#datepicker').datepicker({
+            dateFormat: 'yy-mm-dd',
+            defaultDate: defaultdate,
+            beforeShowDay: datepicker_available,
+            onSelect: datepicker_select
+        });
+    }
 
 // this parses the current iterated date and checks if it's the current displayed
 function formatCurrentDate(dateYmd) {
@@ -21,11 +46,6 @@ function formatCurrentDate(dateYmd) {
     } else {
         return "dateavailable";
     }
-}
-
-function openlink(dateText, inst) {
-    // this needs to be improved. It needs to replace the string instead of duplicating it
-    window.location = window.location + "&unc_date=" + dateText;
 }
 
 Date.prototype.addMinutes= function(m){
