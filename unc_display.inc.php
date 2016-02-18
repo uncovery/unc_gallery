732f473564c5397b675c66283f8f8789093cf765
uncovery <minecraft@uncovery.me>
2016-02-18 14:09:09 +0800

    fix display function

--- unc_display.inc.php
+++ unc_display.inc.php
@@ -16,40 +16,28 @@ function unc_images_display() {
  * the content further. Then calls the function that creates the actual content
  * and returns the modified content
  *
- * @param string $content
+ * @param string $atts
  * @return string
  */
-function unc_gallery($content) {
+function unc_gallery_apply($atts) {
     // this is the REGEX to check only for the activator, other stuff is done later
-    $pattern_activator = "/\[(?'activator'unc_gallery) (?'modifiers'.*)\]/";
-    $base_matches = false;
-    preg_match($pattern_activator, $content, $base_matches);
 
-    // if we cannot even find unc_gallery just return back the content
-    if (!isset($base_matches['activator'])) {
-        return $content;
-    }
+    $a = shortcode_atts( array(
+        'type' => 'day',
+        'date' => 'latest',
+        'file' => 'latest',
+        'options' => false,
+    ), $atts );
 
-    // get all the individual key="value1 value2" patterns:
-    $modifier_matches = false;
-    $groups_pattern = '/( [a-z="]*)/gi';
-    preg_match($groups_pattern, $base_matches['modifiers'], $modifier_matches);
-
-    // now we split them into key=array(value1, value2)
-    $sub_group_pattern = "/(?'key'[\w]*)=\"(?'values'[ \w]*)\"/";
-    $settings = array();
-    foreach ($modifier_matches as $match) {
-        $final_matches = false;
-        preg_match($sub_group_pattern, $match, $final_matches);
-        $temp_key = $final_matches['key'];
-        $temp_values = explode(" ", $final_matches['values']);
-        $settings[$temp_key] = $temp_values;
-    }
+    $type = $a['type'];
+    $date = $a['date'];
+    $file = $a['file'];
+    $options = $a['options'];
 
     // options for displays
     $keywords = array(
         'type' => array(
-            'gallery' => array('datepicker'), // shows a single date's gallery, optional date picker
+            'day' => array('datepicker'), // shows a single date's gallery, optional date picker
             'image' => array('link'), // only one image, requires file addon unless random or latest
             'icon' => array('link'), // only the icon of one image, requires file addon unless random or latest
         ),
@@ -57,26 +45,16 @@ function unc_gallery($content) {
         'file', // in case of image or icon type, you can chose one filename
     );
 
-    // type, we defauly to 'day' if not given or invalid token
-    if (!isset($settings['type']) || isset($settings['type'], $keywords['type'])) {
-        $type = 'day';
-    } else {
-        $type = $settings['type'];
-    }
-
     // icon or image?
     $thumb = false;
     if ($type == 'icon') {
         $thumb = true;
     }
 
-    // date, we default to latest
-    if (!isset($settings['date'])) {
-        $date = 'latest';
-    } else if (in_array($settings['date'], $keywords['date'])) {
-        $date = $keywords['date']; // either latest or random
+    if (in_array($date, $keywords['date'])) {
+        $date = $keywords['date'][$date]; // either latest or random
     } else { //if none of the defaults, we assume date
-        $datetime = new DateTime($settings['date']);
+        $datetime = new DateTime($date);
         if (!$datetime) { // invalid date, fallback to latest
             $date = 'latest'; // TODO: this should throw an error
         } else { // otherwise accept it, format it again to be sure
@@ -88,49 +66,41 @@ function unc_gallery($content) {
         $date = unc_tools_date_latest();
     } elseif ($date == 'random') {
         $date = unc_tools_date_random();
-    }
-
-    // options
-    $possible_type_options = $keywords['type'][$type];
-    if (!isset($settings['options']) || in_array($settings['options'], $possible_type_options)) {
-        $error = "You have an invalid option for the display type $type in your tag";
-        return $error;
-    }
-
-    $link = false;
-    if (in_array('link', $settings['options'])) {
-        $link = true;
-    }
-
-    $datepicker = false;
-    if (in_array('datepicker', $settings['options'])) {
-        $datepicker = true;
-    }
-
-    // date
-    if (!isset($settings['date'])) {
-        return false;
-    }
-    if ($settings['date'] == 'random') {
-        $date = unc_tools_date_random();
-    } else if ($settings['date'] == 'random') {
-        $date = unc_tools_date_latest();
     } else {
         $date = unc_tools_date_validate($date);
     }
 
+
+    // options
+    $possible_type_options = $keywords['type'][$type];
+    if ($options && !in_array($options, $possible_type_options)) {
+        $error = "ERROR: You have an invalid option for the display type \"$type\" in your tag."
+            . "<br>Valid options are: " . implode(",", $keywords['type'][$type]);
+        return $error;
+    }
+
+    $link = false;
+    if (in_array('link', $options)) {
+        $link = true;
+    }
+
+    $datepicker = false;
+    if (in_array('datepicker', $options)) {
+        $datepicker = true;
+    }
+
     if ($type == 'day') {
-        $content_new = unc_gallery_display_page($content, $date, $datepicker);
+        $content_new = unc_gallery_display_page($date, $datepicker);
     } else {
-        $content_new = unc_gallery_display_image($content, $date, $thumb, $link, $settings['file']);
+        $content_new = unc_gallery_display_image($date, $thumb, $link, $file);
     }
 
     // now we got the variables, let's get the actual content
-    $content_new = unc_gallery_display_page($content, $date);
+    $content_new = $out . unc_gallery_display_page($date);
     return $content_new;
 }
 
-function unc_gallery_display_page($content, $date = false) {
+function unc_gallery_display_page($date = false) {
     global $WPG_CONFIG;
 
     // do not let wp manipulate linebreaks
@@ -155,7 +125,7 @@ function unc_gallery_display_page($content, $date = false) {
     if ($date) {
         $date_check = date_create($date);
         if (!$date_check) {
-            return "ERROR: Date not found";
+            return "ERROR: Date '$date' not found";
         }
         $requested_date = $date_check;
     } else if (isset($s_get['unc_date'])) {
@@ -204,9 +174,7 @@ function unc_gallery_display_page($content, $date = false) {
         </div>";
 
     // remove the page tag from the original content and insert the new content
-    $pattern = '/(\[unc_gallery.*\])/';
-    $new_content = preg_replace($pattern, $out, $content);
-    return $new_content;
+    return $out;
 }
 
 function unc_display_folder_list($base_folder) {
@@ -251,6 +219,7 @@ function unc_display_folder_images($date_str) {
     // $curr_photo_folder = $photo_folder . "/" . $date_str;
     $curr_thumb_folder = $thumb_folder . DIRECTORY_SEPARATOR . $date_str;
 
+    $out = '';
     foreach (glob($curr_thumb_folder.DIRECTORY_SEPARATOR."*") as $file) {
         $filename = basename($file);
         if ($file != '.' && $file != '..') {
