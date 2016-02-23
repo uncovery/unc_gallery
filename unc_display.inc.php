65d81a6d37c708505fa0a7f0191f3db11d99e1a7
uncovery <minecraft@uncovery.me>
2016-02-23 18:29:57 +0800

    error message fix

--- unc_display.inc.php
+++ unc_display.inc.php
@@ -193,8 +193,8 @@ function unc_display_single_image($date_str, $file_name) {
     global $UNC_GALLERY;
     $photo_folder =  WP_CONTENT_DIR . $UNC_GALLERY['upload'] . $UNC_GALLERY['photos'];
     $curr_photo_file = $photo_folder . DIRECTORY_SEPARATOR . $date_str . DIRECTORY_SEPARATOR . $file_name;
+    $file_url = $photo_url = content_url($UNC_GALLERY['upload'] . $UNC_GALLERY['photos'] . "/$date_str/$curr_photo_file");
     if (file_exists($curr_photo_file)) {
-        $file_url = $photo_url = content_url($UNC_GALLERY['upload'] . $UNC_GALLERY['photos'] . "/$date_str/$curr_photo_file");
         $out = "<img class=\"featured_image\" alt=\"$file_name\" src=\"$file_url\">\n";
         return $out;
     } else {
