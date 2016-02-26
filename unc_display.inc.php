de5e7a1d05a73d16ef8a0985a326047b82b4cb2c
uncovery <minecraft@uncovery.me>
2016-02-26 15:33:46 +0800

    id instead of class

--- unc_display.inc.php
+++ unc_display.inc.php
@@ -189,7 +189,7 @@ function unc_gallery_display_page($date, $date_selector, $date_desc, $featured_i
         <div class=\"unc_gallery\">
             $datepicker_div
             $delete_link
-            <div class=\"photos\">
+            <div id=\"photos\">
     $single_photo
     $images
             </div>
