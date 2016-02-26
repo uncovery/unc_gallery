8bbd1bc4b740ad68e01aa818230e8a2a6bcdfa79
uncovery <minecraft@uncovery.me>
2016-02-26 18:00:20 +0800

    fix classes

--- unc_display.inc.php
+++ unc_display.inc.php
@@ -226,9 +226,9 @@ function unc_display_folder_images($date_str = false, $skip_file = false, $range
     $out = '';
     if (is_admin()) {
         $out .= "
-        <span class=\"delete_link\">
+        <span class=\"delete_folder_link\">
             Sample shortcode for this day: <input type=\"text\" value=\"[unc_gallery date=&quot;$date_str&quot;]\">
-            <a class=\"delete_folder_link\" href=\"?page=unc_gallery_admin_view&amp;folder_del=$date_str\">
+            <a href=\"?page=unc_gallery_admin_view&amp;folder_del=$date_str\">
                 Delete Date: $date_str
             </a>
         </span>\n";
