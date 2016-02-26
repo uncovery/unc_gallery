68e7c2f7059c11161e818bb8737d193f77348421
uncovery <minecraft@uncovery.me>
2016-02-26 12:44:31 +0800

    make image frame

--- unc_display.inc.php
+++ unc_display.inc.php
@@ -264,9 +264,12 @@ function unc_display_folder_images($date_str = false, $skip_file = false, $range
 
     foreach ($files as $file_date => $file_name) {
         if (is_admin()) {
-            $description .= "<a class=\"delete_image_link\" href=\"?page=unc_gallery_admin_view&amp;image_del=$date_str&amp;filename=$file_name\">Delete Image</a></div>\n";
+            $out .= "<div>";
         }
         $out .= unc_display_single_image($date_str, $file_name, true, $file_date, $description);
+        if (is_admin()) {
+            $out .= "<a class=\"delete_image_link\" href=\"?page=unc_gallery_admin_view&amp;image_del=$date_str&amp;filename=$file_name\">Delete $file_name</a></div>\n";
+        }
     }
 
     if ($echo) {
