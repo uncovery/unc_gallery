7e6f095361f721222ce3a50369a460624e995182
uncovery <minecraft@uncovery.me>
2016-03-15 10:29:54 +0800

    add IPCT manipulation class

--- unc_tools.inc.php
+++ unc_tools.inc.php
@@ -432,3 +432,102 @@ function unc_tools_date_path($date) {
     }
     return $date_str;
 }
+
+/**
+ * Class to write IPTC data to a file
+ * Source: http://stackoverflow.com/questions/5384962/writing-exif-data-in-php
+ *
+ * Example:
+ *  $file = "photo.jpg";
+    $objIPTC = new IPTC($file);
+
+    //set title
+    $objIPTC->setValue(IPTC_HEADLINE, "A title for this picture");
+    echo $objIPTC->getValue(IPTC_HEADLINE);
+ *
+ */
+
+define("IPTC_CREATED_DATE", "055");
+define("IPTC_CREATED_TIME", "060");
+
+
+class IPTC {
+    var $meta = [];
+    var $file = null;
+
+    function __construct($filename) {
+        $info = null;
+        $size = getimagesize($filename, $info);
+        if(isset($info["APP13"])) {
+            $this->meta = iptcparse($info["APP13"]);
+        }
+        $this->file = $filename;
+    }
+
+    function getValue($tag) {
+        return isset($this->meta["2#$tag"]) ? $this->meta["2#$tag"][0] : "";
+    }
+
+    function setValue($tag, $data) {
+        $this->meta["2#$tag"] = [$data];
+        $this->write();
+    }
+
+    private function write() {
+        $mode = 0;
+        $content = iptcembed($this->binary(), $this->file, $mode);
+        $filename = $this->file;
+        if(file_exists($this->file))  {
+            unlink($this->file);
+        }
+
+        $fp = fopen($this->file, "w");
+        fwrite($fp, $content);
+        fclose($fp);
+    }
+
+    private function binary() {
+        $data = "";
+
+        foreach(array_keys($this->meta) as $key) {
+            $tag = str_replace("2#", "", $key);
+            $data .= $this->iptc_maketag(2, $tag, $this->meta[$key][0]);
+        }
+
+        return $data;
+    }
+
+    function iptc_maketag($rec, $data, $value) {
+        $length = strlen($value);
+        $retval = chr(0x1C) . chr($rec) . chr($data);
+
+        if($length < 0x8000) {
+            $retval .= chr($length >> 8) .  chr($length & 0xFF);
+        } else {
+            $retval .= chr(0x80) .
+                chr(0x04) .
+                chr(($length >> 24) & 0xFF) .
+                chr(($length >> 16) & 0xFF) .
+                chr(($length >> 8) & 0xFF) .
+                chr($length & 0xFF);
+        }
+
+        return $retval . $value;
+    }
+
+    function dump() {
+        echo "<pre>";
+        print_r($this->meta);
+        echo "</pre>";
+    }
+
+    #requires GD library installed
+    function removeAllTags() {
+        $this->meta = [];
+        $img = imagecreatefromstring(implode(file($this->file)));
+        if(file_exists($this->file)) {
+            unlink($this->file);
+        }
+        imagejpeg($img, $this->file, 100);
+    }
+}
