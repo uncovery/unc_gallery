8065d650b3cb796a7b68727b19f73be0c7465c97
uncovery <minecraft@uncovery.me>
2016-03-24 17:40:49 +0900

    double quote

--- unc_display.inc.php
+++ unc_display.inc.php
@@ -284,7 +284,7 @@ function unc_display_folder_images() {
     $i = 0;
     foreach ($files as $F) {
         if ($F['file_name'] == $D['featured_image']){ 
-            $featured .= "<div class=\"featured_photo\"\">\n"
+            $featured .= "<div class=\"featured_photo\">\n"
                 . unc_display_image_html($F['file_path'], false, $F)
                 . "</div>\n";
         } else {
