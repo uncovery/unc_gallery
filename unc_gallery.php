24841104e7bee6a27fe90f62dfa97add04fd8766
uncovery <minecraft@uncovery.me>
2016-06-21 16:29:37 +0800

    last attempt of bugfix

--- unc_gallery.php
+++ unc_gallery.php
@@ -3,7 +3,7 @@
 Plugin Name: Uncovery Gallery
 Plugin URI:  https://uncovery.net/about
 Description: A simple, self-generating, date-based gallery with bulk uploading
-Version:     4.1.2
+Version:     4.1.3
 Author:      Uncovery
 Author URI:  http://uncovery.net
 License:     GPL2
